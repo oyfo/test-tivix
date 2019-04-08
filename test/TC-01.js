f01b66927750418cb083a756c77be52d2420f74f
Mikolaj <mikolaj.kacprzak@gmail.com>
2019-04-09 01:01:47 +0200

    Updated readme Updated project description, updated TC-01 describe part

--- test/TC-01.js
+++ test/TC-01.js
@@ -10,7 +10,7 @@ const URLHelper = require('./../helpers/URLhelper');
 let config = process.env.npm_config_config || 'tuvix_test_env';
 config = JSON.parse(fs.readFileSync(`${__dirname}/../config/${config}.json`));
 
-describe('Tivix smoke test', () => {
+describe('TC-01 Tivix smoke test', () => {
   const urlHelper = new URLHelper(config);
   const urls = urlHelper.buildFullUrls();
   urls.forEach((url) => {
